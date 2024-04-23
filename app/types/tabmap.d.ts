b4fa5e6643bf777a5399161fc48c968c4b88b6a4
zhuima <zhuima314@gmail.com>
2024-04-23 14:10:05 +0800

    change logo

---
+++ app/types/tabmap.d.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: zhuima zhuima314@gmail.com
+ * @Date: 2024-04-23 14:09:50
+ * @LastEditors: zhuima zhuima314@gmail.com
+ * @LastEditTime: 2024-04-23 14:09:57
+ * @FilePath: /seo/app/types/tabMap.d.ts
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+export interface TabMap {
+  [key: string]: string;
+}
