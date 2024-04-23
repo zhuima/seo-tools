54ed74e814e15a274eb494e90ae37ebfa9d66b61
zhuima <zhuima314@gmail.com>
2024-04-23 13:43:52 +0800

    update cache method

--- app/config/tabMap.ts
+++ app/config/tabMap.ts
@@ -1,6 +1,7 @@
 import { TabMap } from "@/app/types/tabmap";
 
 export const tabMap: TabMap = {
+  一体化SEO工具: "all-in-one-seo-tool",
   博客: "blog",
   关键词研究: "keyword-research",
   内容优化: "content-optimization",
@@ -8,7 +9,6 @@ export const tabMap: TabMap = {
   校验检测: "validator-checker",
   SEO浏览器扩展: "seo-browser-extension",
   SEO分析: "seo-analytics",
-  一体化SEO工具: "all-in-one-seo-tool",
   技术SEO: "technical-seo",
   反向链接分析: "backlink-analysis",
 };
